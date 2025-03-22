2114c9c19f265083fb76002ad0829ec769ee11c6
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-03-22 17:10:09 +0530

    Implemented redis queue using bull and ui using bullAdapter

--- src/services/workspaceService.js
+++ src/services/workspaceService.js
@@ -1,12 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 import { v4 as uuidv4 } from 'uuid';
 
+import { addEmailToMailQueue } from '../producers/mailQueueProducer.js';
 import channelRepository from '../repositories/channelRepository.js';
 import userRepository from '../repositories/userRepository.js';
 import workspaceRepository from '../repositories/workspaceRepository.js';
+import { workspaceJoinMail } from '../utils/common/mailObject.js';
 import ClientError from '../utils/errors/clientError.js';
 import ValidationError from '../utils/errors/validationError.js';
-
 const isUserAdminOfWorkspace = (workspace, userId) => {
   return workspace.members.find(
     (member) =>
@@ -242,6 +243,10 @@ export const addMemberToWorkspaceService = async (
       memberId,
       role
     );
+    addEmailToMailQueue({
+      ...workspaceJoinMail(workspace),
+      to: isValidUser.email
+    });
     return response;
   } catch (error) {
     console.log('Add member to workspace service error', error);
