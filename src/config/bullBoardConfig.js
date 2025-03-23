972378fe906867353ff0f397928c199db46b77af
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-03-23 09:17:13 +0530

    Added member api

--- src/config/bullBoardConfig.js
+++ src/config/bullBoardConfig.js
@@ -1,6 +1,7 @@
 import { createBullBoard } from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter.js';
 import { ExpressAdapter } from '@bull-board/express';
+
 import mailQueue from '../queues/mailQueue.js';
 
 const bullServerAdapter = new ExpressAdapter();
