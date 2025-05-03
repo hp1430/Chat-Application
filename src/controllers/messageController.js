75fc3a526088509a1faafd7923af90926d731ad7
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-05-04 00:34:37 +0530

    Added aws configuration to share images

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
+import { s3 } from '../config/awsConfig.js';
+import { AWS_BUCKET_NAME } from '../config/serverConfig.js';
 import { getMessagesService } from '../services/messageService.js';
 import {
   customErrorResponse,
@@ -22,6 +24,29 @@ export const getMessagesController = async (req, res) => {
       .json(successResponse(messages, 'Messages fetched successfully'));
   } catch (error) {
     console.log('Get messages controller error', error);
+    if (error.statusCode) {
+      return res.status(error.statusCode).json(customErrorResponse(error));
+    }
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(error));
+  }
+};
+
+export const getPresignedUrlFromAWS = async (req, res) => {
+  try {
+    const url = await s3.getSignedUrlPromise('putObject', {
+      Bucket: AWS_BUCKET_NAME,
+      Key: `${Date.now()}`,
+      Expires: 60 //60 seconds
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json(
+        successResponse(url, 'Presigned url generated successfully from AWS')
+      );
+  } catch (error) {
+    console.log('Error in getPresignedUrlFromAWS', error);
     if (error.statusCode) {
       return res.status(error.statusCode).json(customErrorResponse(error));
     }
